9516958a1033f646d588dfe6ed27f66b1000201f
Willow Neilson <64460998+WillowRN@users.noreply.github.com>
2024-02-26 14:05:54 -0800

    Finished drawer limits & made barrier
    Duplicated buildings at the end of the scene to form a barrier and finished the drawer limits scripting.

--- XRInteractions/Assets/Scripts/DrawerInteractable.cs
+++ XRInteractions/Assets/Scripts/DrawerInteractable.cs
@@ -15,6 +15,7 @@ public class DrawerInteractable : XRGrabInteractable
     private const string grabLayer = "Grab";
     private bool isGrabbed;
     private Vector3 limitPositions;
+    [SerializeField] float drawerLimitZ = 0.8f;
     [SerializeField] private Vector3 limitDistances = new Vector3(.02f, .02f, 0 );
     void Start()
     {
@@ -84,7 +85,22 @@ public class DrawerInteractable : XRGrabInteractable
         {
             ChangeLayerMask(defaultLayer);
         }
-       
+       else if(drawerTransform.localPosition.z <= limitPositions.z - limitDistances.z)
+        {
+            isGrabbed = false;
+            drawerTransform.localPosition = limitPositions;
+            ChangeLayerMask(defaultLayer);
+        }
+       else if (drawerTransform.localPosition.z >= drawerLimitZ + limitDistances.z)
+        {
+            isGrabbed = false;
+            drawerTransform.localPosition = new Vector3(
+                drawerTransform.localPosition.x,
+                drawerTransform.localPosition.y,
+                drawerLimitZ);
+            ChangeLayerMask(defaultLayer);
+        }
+
     }
    
 
